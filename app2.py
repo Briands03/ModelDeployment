b0543898509901f7fae854c9a6a697b4f340e9c9
Charlie Sen <89641783+Briands03@users.noreply.github.com>
2025-02-15 03:08:16 +0700

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from ml_app import run_ml_app
+def main():
+    menu = ['Home','Machine Learning']
+    choice = st.sidebar.selectbox('Menu',menu)
+
+    if choice == 'Home':
+        st.subheader('Welcome to Home Page')
+    elif choice == 'Machine Learning':
+        #st.subheader('Welcome to Machine Learning')
+        run_ml_app()
+
+if __name__ == '__main__':
+    main()
